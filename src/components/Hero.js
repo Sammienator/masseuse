ea902362a826940395449ec35a4e4570d0bb7ef2
Sammienator <sammugo18@gmail.com>
2024-04-03 16:31:48 +0300

    changes to my hero section

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,55 +1,65 @@
 import React from 'react';
+import { Container, Button } from 'react-bootstrap';
+import { AiOutlineArrowDown } from 'react-icons/ai';
 import { Carousel } from 'react-bootstrap';
-import pic1 from '../assets/pic1.jpg';
-import pic6 from '../assets/pic6.jpg';
-import pic3 from '../assets/pic3.jpg';
+import 'aos/dist/aos.css';
+import AOS from 'aos';
+import pic11 from '../assets/pic11.jpg';
+import pic22 from '../assets/pic22.jpg';
+import pic33 from '../assets/pic33.jpg';
 
 function HeroSection() {
+  React.useEffect(() => {
+    AOS.init({ duration: 1000 });
+  }, []);
+
   return (
-    <Carousel>
-      <Carousel.Item>
-        <img
-          className="d-block w-100"
-          src={pic1}
-          alt="First slide"
-        />
-        <Carousel.Caption>
-          <h3>Relax and Rejuvenate</h3>
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas
-            sed diam eget risus porta condimentum.
-          </p>
-        </Carousel.Caption>
-      </Carousel.Item>
-      <Carousel.Item>
-        <img
-          className="d-block w-100"
-          src={pic6}
-          alt="Second slide"
-        />
-        <Carousel.Caption>
-          <h3>Reduce Stress and Pain</h3>
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas
-            sed diam eget risus porta condimentum.
-          </p>
-        </Carousel.Caption>
-      </Carousel.Item>
+    <Container fluid className="p-0">
+      <Carousel>
         <Carousel.Item>
           <img
             className="d-block w-100"
-            src={pic3}
-            alt="Third slide"
+            src={pic11}
+            alt="First slide"
+            style={{ height: '100vh', objectFit: 'cover' }}
           />
-          <Carousel.Caption>
-            <h3>Improve Sleep Quality</h3>
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas
-              sed diam eget risus porta condimentum.
-            </p>
-          </Carousel.Caption>
+          <div className="carousel-caption d-flex flex-column justify-content-center align-items-center">
+            <h1 data-aos="fade-up">Welcome to our Spa</h1>
+            <p data-aos="fade-up" className="mt-3">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+            <Button variant="primary" data-aos="fade-up" className="mt-3">Learn More</Button>
+          </div>
         </Carousel.Item>
-    </Carousel>
+        <Carousel.Item>
+          <img
+            className="d-block w-100"
+            src={pic22}
+            alt="Second slide"
+            style={{ height: '100vh', objectFit: 'cover' }}
+          />
+          <div className="carousel-caption d-flex flex-column justify-content-center align-items-center">
+            <h1 data-aos="fade-up">Relax and Unwind</h1>
+            <p data-aos="fade-up" className="mt-3">Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium.</p>
+            <Button variant="primary" data-aos="fade-up" className="mt-3">Book Now</Button>
+          </div>
+        </Carousel.Item>
+        <Carousel.Item>
+          <img
+            className="d-block w-100"
+            src={pic33}
+            alt="Third slide"
+            style={{ height: '100vh', objectFit: 'cover' }}
+          />
+          <div className="carousel-caption d-flex flex-column justify-content-center align-items-center">
+            <h1 data-aos="fade-up">Pamper Yourself</h1>
+            <p data-aos="fade-up" className="mt-3">At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias.</p>
+            <Button variant="primary" data-aos="fade-up" className="mt-3">Contact Us</Button>
+          </div>
+        </Carousel.Item>
+      </Carousel>
+      <div className="scroll-down-indicator text-center">
+        <AiOutlineArrowDown size={32} data-aos="fade-up" data-aos-offset="200" data-aos-duration="1000" />
+      </div>
+    </Container>
   );
 }
 
